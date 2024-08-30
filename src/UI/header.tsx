581fbd14d94411bb97da69921aa14e645f91ba23
hildeandantas <hildeandantas@gmail.com>
2024-08-30 16:23:35 -0300

    feat: Added header but font wheight look so big

---
+++ src/UI/header.tsx
@@ -0,0 +1,14 @@
+import '../UI/styles/header.css';
+
+
+// Only a preview, this is not the actual header
+export default function Header() {
+  return (
+    <div className='headerContainer'>
+      <h1>Home</h1>
+      <h1>Contas</h1>
+      <h1>Receitas</h1>
+      <h1>Despesas</h1>
+    </div>
+  );
+}
