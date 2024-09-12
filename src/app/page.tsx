fc481897395e7ddb86b3cc9016f0077125d5f4ba
Gabriel Vilela <gabriel.vilela@gmail.com>
2024-09-12 18:25:11 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,27 @@
 'use client'
 import ContainerContas from '@/components/containerContas'
 import LogoutButton from '@/UI/logoutButton'
-import { useAuth0 } from '@auth0/auth0-react'
+import { Auth0Context, useAuth0 } from '@auth0/auth0-react'
 import LoadingScreen from '@/UI/loadingScreen'
 import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
+import { useContext, useEffect } from 'react'
 
 function Home() {
   const router = useRouter()
 
   const { isAuthenticated, isLoading } = useAuth0()
+
+  const { user, logout } = useContext(Auth0Context)
+
   useEffect(() => {
     if (!isAuthenticated && !isLoading) {
       router.push('/login')
     }
-  }, [isAuthenticated, isLoading, router])
+
+    if (isAuthenticated) {
+      console.log(user)
+    }
+  }, [isAuthenticated, isLoading, router, user])
   return (
     <main>
       <div>
